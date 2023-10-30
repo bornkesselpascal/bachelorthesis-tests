923d318ce1910d4d41d2046e6b9280631875b807
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-10-30 06:59:13 +0000

    Changed format_query to not change the original query

--- parser/eParser/data_format.py
+++ parser/eParser/data_format.py
@@ -5,29 +5,30 @@ def format_query(query: list, duartion: float, losses: int, total: int) -> list:
     #   - This means that the messages may not arrive in the correct order (the query message is very small).
     #   - This function fixes the data by looking for negative differences.
 
-    query.append({'losses': losses, 'total': total, 'timestamp': duartion, 'difference': (losses - query[-1]['losses'])})
-    if  not any(report['difference'] < 0 for report in query):
+    new_query = query.copy()
+    new_query.append({'losses': losses, 'total': total, 'timestamp': duartion, 'difference': (losses - new_query[-1]['losses'])})
+    if  not any(report['difference'] < 0 for report in new_query):
         # No negative differences found, return the query as is
-        return query
+        return new_query
     
     # Negative differences found, we need to fix the data.
     
     # Iterate through the list and until we find a negative difference
-    while any(report['difference'] < 0 for report in query):
-        for idx, current_report in enumerate(query):
+    while any(report['difference'] < 0 for report in new_query):
+        for idx, current_report in enumerate(new_query):
             if current_report['difference'] < 0:
                 # Found a negative difference, fix the data
-                for previous_report in query[:idx]:
+                for previous_report in new_query[:idx]:
                     # If the previous report has more losses than the current report, we can fix the data
                     if previous_report['losses'] > current_report['losses']:
                         previous_report['losses'] = current_report['losses']
                 break
 
         # Recalculate the differences
-        for idx, current_report in enumerate(query[:idx+1]):
+        for idx, current_report in enumerate(new_query[:idx+1]):
             if idx == 0:
                 current_report['difference'] = current_report['losses']
             else:
-                current_report['difference'] = current_report['losses'] - query[idx-1]['losses']
+                current_report['difference'] = current_report['losses'] - new_query[idx-1]['losses']
 
-    return query
+    return new_query
